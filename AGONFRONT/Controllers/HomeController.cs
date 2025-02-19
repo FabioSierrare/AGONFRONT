946a284192e9a1ba331e276ea2cddc584ac1670c
danielpa512 <danielparraayala0@gmail.com>
2025-02-18 19:32:03 -0500

    Login

--- AGONFRONT/Controllers/HomeController.cs
+++ AGONFRONT/Controllers/HomeController.cs
@@ -22,19 +22,6 @@ namespace AGONFRONT.Controllers
             return View();
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
         public ActionResult LoginPartial()
         {
             return PartialView("_LoginPartial");
