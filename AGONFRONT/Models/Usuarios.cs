cb207de3a17a725699b058c594f7fdfe195e79c0
Daniel20 <Daniel20@DANIEL-VARGAS20>
2025-02-21 14:49:04 -0500

    Modificamos registro de usuario y su controlador

--- AGONFRONT/Models/Usuarios.cs
+++ AGONFRONT/Models/Usuarios.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations.Schema;
+using Newtonsoft.Json;
 
 
 namespace AGONFRONT.Models
@@ -16,18 +17,25 @@ namespace AGONFRONT.Models
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
 
-        [Display(Name = " Nombre")]
+        [Display(Name = " Nombre Completo")]
         [Required(ErrorMessage = " El campo {0} es requerido")]
         public string Nombre { get; set; }
+
 
         [Display(Name = " Correo")]
         [EmailAddress(ErrorMessage = " El email no tiene un formato válido")]
         [Required(ErrorMessage = " El campo {0} es requerido")]
         public string Correo { get; set; }
 
-        [Display(Name = " Contraseña")]
-        [Required(ErrorMessage = " El campo {0} es requerido")]
+        [Required(ErrorMessage = "La contraseña es obligatoria.")]
+        [DataType(DataType.Password)]
         public string Contraseña { get; set; }
+
+        [Required(ErrorMessage = "Debe confirmar la contraseña.")]
+        [DataType(DataType.Password)]
+        [JsonIgnore]
+        [Compare("Contraseña", ErrorMessage = "Las contraseñas no coinciden.")]
+        public string ConfirmarContraseña { get; set; }
 
         [Display(Name = " Telefono")]
         [Required(ErrorMessage = " El campo {0} es requerido")]
@@ -39,6 +47,18 @@ namespace AGONFRONT.Models
 
         [Display(Name = " Tipo de usuario")]
         [Required(ErrorMessage = " El campo {0} es requerido")]
+        public string TipoDocumento { get; set; }
+
+        [Display(Name = " Documento")]
+        [Required(ErrorMessage = " El campo {0} es requerido")]
+
+        public int Documento { get; set; }
+
+
+        [Display(Name = " Tipo de usuario")]
+        [Required(ErrorMessage = " El campo {0} es requerido")]
+
+
         public string TipoUsuario { get; set; }
 
         [Display(Name =  "Fehca de creacion")]
