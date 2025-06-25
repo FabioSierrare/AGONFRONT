7bffe2955d50783d467aef9a49540abf03bd981d
Daniel20 <Daniel20@DANIEL-VARGAS20>
2025-06-25 16:30:16 -0500

    Se hace commit de la validacion admin productos

--- AGONFRONT/Models/Productos.cs
+++ AGONFRONT/Models/Productos.cs
@@ -1,11 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace AGONFRONT.Models
 {
@@ -15,36 +10,42 @@ namespace AGONFRONT.Models
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
 
-        [Required(ErrorMessage = " El campo {0} es requerido")]
-        [Display(Name = " Nombre")]
+        [Required(ErrorMessage = "El campo {0} es requerido")]
+        [Display(Name = "Nombre")]
+        [RegularExpression(@"^[a-zA-Z0-9\s]+$", ErrorMessage = "El nombre solo puede contener letras, números y espacios.")]
         public string Nombre { get; set; }
 
-        [Display(Name = " Descripcion del producto")]
+        [Display(Name = "Descripción del producto")]
         public string Descripcion { get; set; }
 
-        [Required(ErrorMessage = " El campo {0} es requerido")]
-        [Display(Name = " Precio del producto")]
+        [Required(ErrorMessage = "El campo {0} es requerido")]
+        [Display(Name = "Precio del producto")]
+        [Range(0.01, 9999999, ErrorMessage = "El precio debe ser mayor que cero.")]
+        [RegularExpression(@"^\d+(\.\d{1,2})?$", ErrorMessage = "Formato de precio inválido. Use hasta dos decimales.")]
         public decimal Precio { get; set; }
 
-        [Required(ErrorMessage = " El campo {0} es requerido")]
-        [Display(Name = " Stock del producto")]
+        [Required(ErrorMessage = "El campo {0} es requerido")]
+        [Display(Name = "Stock del producto")]
+        [Range(0, int.MaxValue, ErrorMessage = "El stock debe ser un número entero positivo.")]
         public int Stock { get; set; }
 
-        [Required(ErrorMessage = " El campo {0} es requerido")]
-        [Display(Name =  "Fecha de creacion")]
+        [Required(ErrorMessage = "El campo {0} es requerido")]
+        [Display(Name = "Fecha de creación")]
         [DataType(DataType.Date)]
         public DateTime FechaCreacion { get; set; }
 
-        [Display(Name = " Id de la Categoria")]
+        [Required(ErrorMessage = "Debe seleccionar una categoría.")]
+        [Display(Name = "Id de la Categoría")]
         public int CategoriaId { get; set; }
 
-        [Display(Name = " Id del Vendedor")]
+        [Required(ErrorMessage = "Debe seleccionar un vendedor.")]
+        [Display(Name = "Id del Vendedor")]
         public int VendedorId { get; set; }
 
-        [Display(Name = " UrlImagen")]
+        [Display(Name = "Url de la imagen")]
         public string UrlImagen { get; set; }
 
-        [NotMapped] // Importante si esta propiedad no viene desde la base de datos, sino del JSON
+        [NotMapped]
         public Categoria Categoria { get; set; }
     }
 }
